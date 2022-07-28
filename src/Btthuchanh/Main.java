196f4f6223439fb751f0dbe5545f906aa05ace57
unknown <tavanhuyvp8590@gmail.com>
2022-07-29 05:25:15 +0700

    HUY

---
+++ src/Btthuchanh/Main.java
@@ -0,0 +1,13 @@
+package Btthuchanh;
+
+import rikkei.academy.Car;
+
+public class Main {
+ public static void main(String[] args) {
+Car car = new Car("mada 3","toyota vios");
+  System.out.println(car.numberofCars);
+  Car car2 = new Car("Mada 2", "toyota camry");
+  System.out.println(car.numberofCars);
+
+ }
+}
---
+++ src/rikkei/academy/Car.java
@@ -0,0 +1,15 @@
+
+       package rikkei.academy;
+        public class Car {
+         public boolean numberofCars;
+         private String name;
+ private String engine;
+ public static int numberOfCars;
+ public Car(String name, String engine) {
+ this.name = name;
+ this.engine = engine;
+numberOfCars++;
+}
+
+// getters and setters
+ }
